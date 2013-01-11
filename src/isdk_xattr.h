a2d48be0814f3ac2ef835f097af0b4000a645b7b
Riceball LEE <snowyu.lee@gmail.com>
2013-01-11 23:32:28 +0800

    * adjust year for doc

--- src/isdk_xattr.h
+++ src/isdk_xattr.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2012 Riceball LEE(snowyu.lee@gmail.com)
+  Copyright (c) 2012-2013 Riceball LEE(snowyu.lee@gmail.com)
  
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
