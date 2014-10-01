5956f6c00264de4d8beead3ecf679eba99a42d95
Riceball LEE <snowyu.lee@gmail.com>
2014-10-01 22:18:16 +0800

    * update to new zmalloc.c version

--- src/deps/zmalloc.h
+++ src/deps/zmalloc.h
@@ -72,8 +72,10 @@ void zfree(void *ptr);
 char *zstrdup(const char *s);
 size_t zmalloc_used_memory(void);
 void zmalloc_enable_thread_safeness(void);
-float zmalloc_get_fragmentation_ratio(void);
+void zmalloc_set_oom_handler(void (*oom_handler)(size_t));
+float zmalloc_get_fragmentation_ratio(size_t rss);
 size_t zmalloc_get_rss(void);
+size_t zmalloc_get_private_dirty(void);
 void zlibc_free(void *ptr);
 
 #ifndef HAVE_MALLOC_SIZE
